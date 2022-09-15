cabb95b06b427b9399b1020fd75c74705dbf77e1
21327 <rodrigo.villegaszambrano@vtsd.com>
2022-09-15 10:23:43 -0400

    finish

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,14 +1,23 @@
-/*
-    Welcome to your first lab!  All work should be completed inside of this file.
-    Please make sure that as you work on your lab you save, commit, and push everyday.
+//Lab02v80.java
+//The 80-point version of Lab 02 is an exercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file.
 
-    As always, if you have any questions reach out and email me!
 
-*/
-
-  public class Lab02vst {
-
-    public static void main(String args[]){
-
+public class Lab02vst {
+    public static void main(String[] args) {
+        System.out.println("lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####    ##### ##### # #   # ##### #####");
+        System.out.println("# #   # #   #    #   # #   # # ##  #   #   #    ");
+        System.out.println("# #   # #   #    ##### #   # # # # #   #   #####");
+        System.out.println("# #   # #   #    #     #   # # #  ##   #       #");
+        System.out.println("# ##### #####    #     ##### # #   #   #   #####");
     }
-  }
+}
